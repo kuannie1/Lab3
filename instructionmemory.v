34fa638da00cfa356497aad7f74f95ee1949988d
Prava Dhulipalla <prava@students.olin.edu>
2017-11-15 02:39:19 -0500

    instruction memory module complete

---
+++ instructionmemory.v
@@ -0,0 +1,19 @@
+/*
+  Module for instruction memory.
+  Based on Ben Hill's implementation of a basic memory.
+*/
+
+module instructionmemory
+(
+  input clk
+  input[9:0] Addr,
+  output[31:0]  DataOut
+);
+  
+  reg [31:0] mem[1023:0];  
+  
+  // change filename based on assembly .dat file
+  initial $readmemh(“file.dat”, mem);
+    
+  assign DataOut = mem[Addr];
+endmodule
