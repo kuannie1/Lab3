27004114075425fa5c6901dae59d79650049ebf1
Prava Dhulipalla <prava@students.olin.edu>
2017-11-25 20:31:53 -0500

    stash

--- alu.t.v
+++ alu.t.v
@@ -1,5 +1,5 @@
-// `include "alu.v"
-`include "ALU/adder.v"
+`include "alu.v"
+// `include "ALU/adder.v"
 
 `define opADD  3'd0
 `define opSUB  3'd1
@@ -17,34 +17,34 @@ module alu_test ();
 	reg [31:0] operandB;
 	reg [2:0] command;
 
-	// ALU alu(.result(result), .carryout(carryout), .zero(zero),
-	// 	.overflow(overflow), .operandA(operandA), .operandB(operandB), 
-	// 	.command(command));
+	ALU alu(.result(result), .carryout(carryout), .zero(zero),
+		.overflow(overflow), .operandA(operandA), .operandB(operandB), 
+		.command(command));
 
-	Subtractor32bit get_sub_out(operandA, operandB, result, carryout, overflow);
+	// Subtractor32bit get_sub_out(operandA, operandB, result, carryout, overflow);
 	// FullAdder32bit add(result, carryout, overflow, operandA, operandB);
 
 
 	reg [3:0] opA;
 	reg [3:0] opB;
 	wire [3:0] res;
-	FullAdder4bit newadd(res, carryout, overflow, opA, opB, 1'b0);
+	// FullAdder4bit newadd(res, carryout, overflow, opA, opB, 1'b0);
 
 
 
 	initial begin
 		$dumpfile("alu.vcd");
 		$dumpvars();
-		opA = 4'b1111; opB = 4'b1010; #1000
-		$display("opA: %b", opA);
-		$display("opB: %b", opB);
-		$display("sum: %b", res);
+		// opA = 4'b1111; opB = 4'b1010; #1000
+		// $display("opA: %b", opA);
+		// $display("opB: %b", opB);
+		// $display("sum: %b", res);
 
 		// operandA = 32'd20; operandB = 32'b11111111111111111111111111111011; #1500 // command = `opSUB; #1500
-		operandA = 32'd20; operandB = 32'd5; #1500
-		$displayb(operandA);
-		$displayb(operandB);
-		$display("result: %b", result);
+		operandA = 32'd20; operandB = 32'd20; command = `opCNE; #1500
+		$display(operandA);
+		$display(operandB);
+		$display("result: %d", result);
 		// $displayb("a: 		%b", get_sub_out.add1tob.add4.add1.a);
 		// $displayb("b: 		%b", get_sub_out.add1tob.add4.add1.b);
 		// $displayb("carryin: 	%b", get_sub_out.add1tob.add4.add1.carryin);
