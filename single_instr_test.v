8ecf26f41efafaaed867c6ebfe2b4a6e934d107c
Xiaozheng Xu <xiaozhengxu@Xiaozhengs-MacBook-Air.local>
2017-12-05 11:39:21 -0500

    minor changes

--- single_instr_test.v
+++ single_instr_test.v
@@ -24,7 +24,7 @@ module instr_test();
     initial begin
     // Dump waveforms to file
     // Note: arrays (e.g. memory) are not dumped by default
-    $dumpfile("sw.vcd");
+    $dumpfile("jal.vcd");
     $dumpvars();
 
 
